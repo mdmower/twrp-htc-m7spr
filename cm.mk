259b0af5fab53341256b5732d2ac70a94679de5b
Matt Mower <mowerm@gmail.com>
2014-03-04 18:36:49 -0600

    Create m7spr recovery only tree

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := m7vzw
+PRODUCT_RELEASE_NAME := m7spr
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/m7vzw/device_m7vzw.mk)
+$(call inherit-product, device/htc/m7spr/device_m7spr.mk)
 
 # Device identifier - this must come after all inclusions
-PRODUCT_DEVICE := m7vzw
-PRODUCT_NAME := cm_m7vzw
+PRODUCT_DEVICE := m7spr
+PRODUCT_NAME := cm_m7spr
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One
 PRODUCT_MANUFACTURER := HTC
